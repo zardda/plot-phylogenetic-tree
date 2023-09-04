de58acca00bfc60bc679ccee29a66d2855601cec
zardda <79556884+zardda@users.noreply.github.com>
2023-09-04 17:03:42 +0800

    Update example.py

--- example.py
+++ example.py
@@ -20,10 +20,3 @@ ax2 = fig.add_subplot(gs[1,1],projection='polar')
 plot_polar_tree(root,ax2,color={"node_1":"red","node_5":"blue","node_2":"green"},align_name=True,add_label=True)
 
 plt.show()
-# tree_str = "((((FgRID1:0.48420347,NcRID1:0.57779907):0.20194337,Masc1:0.58826710):0.25546475,(DNMT2:0.92001531,Masc2:0.90135602):0.00743693,DNMT1:0.91475001):0.06125499,(Masc3:0.70355473,(NcDIM2:0.46362493,FgDIM2:0.43955339):0.18145267):0.43415809)root;"
-# root = parse_newick(tree_str)
-# #plot_tree(root,color={"node_1":"red","node_5":"blue","node_2":"green"},align_name=False)
-# # print(get_parent_node(root))
-# fig,ax = plt.subplots(subplot_kw={'projection': 'polar'})
-# plot_polar_tree(root,ax,color={"node_1":"red","node_5":"blue","node_2":"green"})
-# plt.show()
